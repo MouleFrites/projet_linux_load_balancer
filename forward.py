d68ba38f3ed1090105225c75c4b0476f1928a81f
root <root@localhost.localdomain>
2018-11-26 17:52:03 +0100

    patch_1

--- forward.py
+++ forward.py
@@ -18,7 +18,7 @@ elif sys.argv[1] == "-a":
 	bashCommand = "iptables -F | iptables -X | iptables -t filter -A FORWARD -i enp0s9 -o enp0s3 -j ACCEPT | iptables -t filter -A FORWARD -i enp0s3 -o enp0s9 -j ACCEPT | iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE"
 	exeBash(bashCommand)
 elif sys.argv[1] == "-d":
-	bashCommand = "echo '1' > /proc/sys/net/ipv4/ip_forward"
+	bashCommand = "echo '0' > /proc/sys/net/ipv4/ip_forward"
 	exeBash(bashCommand)
 	bashCommand = "iptables -F | iptables -X"
 	exeBash(bashCommand)
