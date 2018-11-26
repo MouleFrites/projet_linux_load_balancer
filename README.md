85bc6fce3fdfd63766524df26d7927d2eba9353f
root <root@localhost.localdomain>
2018-11-26 17:25:59 +0100

    first-commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# projet_linux_load_balancer
---
+++ forward.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3.6
+#MoulesFrites
+#26/11/2018
+#Active/desactive le forward sur centos
+
+import subprocess
+import sys
+
+def exeBash(bash_Command):
+	output = subprocess.check_output(['bash','-c', bash_Command])
+
+
+if len(sys.argv) < 2:
+	print('Use -a for activate or -d to disable')
+elif sys.argv[1] == "-a":
+	bashCommand = "echo '1' > /proc/sys/net/ipv4/ip_forward"
+	exeBash(bashCommand)
+	bashCommand = "iptables -F | iptables -X | iptables -t filter -A FORWARD -i enp0s9 -o enp0s3 -j ACCEPT | iptables -t filter -A FORWARD -i enp0s3 -o enp0s9 -j ACCEPT | iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE"
+	exeBash(bashCommand)
+elif sys.argv[1] == "-d":
+	bashCommand = "echo '1' > /proc/sys/net/ipv4/ip_forward"
+	exeBash(bashCommand)
+	bashCommand = "iptables -F | iptables -X"
+	exeBash(bashCommand)
+
